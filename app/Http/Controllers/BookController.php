0f658c3ec5cf5d7c917ae9e6642f0752af83edae
vietanh123456 <23010117@st.phenikaa-uni.edu.vn>
2025-10-16 23:05:11 +0700

    update bookctroller

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    //
+    // Hiển thị danh sách sách
+    public function index()
+    {
+        return Book::all();
+    }
+
+    // Xem chi tiết 1 sách
+    public function show($id)
+    {
+        return Book::findOrFail($id);
+    }
+
+    // Thêm sách mới
+    public function store(Request $request)
+    {
+        $book = Book::create($request->all());
+        return response()->json($book, 201);
+    }
+
+    // Cập nhật thông tin sách
+    public function update(Request $request, $id)
+    {
+        $book = Book::findOrFail($id);
+        $book->update($request->all());
+        return response()->json($book, 200);
+    }
+
+    // Xóa sách
+    public function destroy($id)
+    {
+        Book::destroy($id);
+        return response()->json(null, 204);
+    }
 }
